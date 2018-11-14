3890a8397deebf6a4f1f3cf52e6b435a9bb09e8d
Axel <axdahlin@gmail.com>
2018-11-14 15:45:34 +0100

    add begining of chart

--- starter-code/public/javascripts/financial-data.js
+++ starter-code/public/javascripts/financial-data.js
@@ -1,42 +1,10 @@
-
-
 axios.get('http://api.coindesk.com/v1/bpi/historical/close.json')
   .then(response =>{
     printTheChart(response.data.bpi)
-
-    console.log(response.data.bpi)
-
   })
-
-
 
 var ctx = document.getElementById("myChart").getContext('2d');
 
-
-
-
-// const stockInfo  = axios.create({
-//   baseURL: 'https://api.iextrading.com/1.0/stock/',
-// });
-
-// let stockTicket = "amzn";
-
-// stockInfo.get(`${stockTicket}/chart`)
-//   .then(response => {
-//     console.log(response);
-//   })
-//   .catch(error => {
-//     console.log(error);
-// });
-
-
-// stockInfo.get(`${stockTicket}/chart`)
-//     .then(response => {
-//       printTheChart(response.data);
-//     })
-//     .catch( error => {
-//       console.log(error);
-//   });
 
 
 
@@ -46,15 +14,9 @@ function printTheChart (data) {
   const label = Object.keys(data);
   const costs = Object.values(data);
 
-
-  // let stockLabels = stockData.map( element => element.date);
-  // let stockPrice = stockData.map( element => element.close);
-
-
-
   let ctx = document.getElementById('myChart').getContext('2d');
   let chart = new Chart(ctx, {
-    type: 'line',
+    type: 'pie',
     data: {
       labels: label,
       datasets: [{
